807d0b6c2b02dd14107ff40c9e3de55db01d1721
Munim Ali <munim15@gmail.com>
2014-11-01 22:48:09 -0700

    Finished up to part 4. Still need to answer question 2b

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -19,11 +19,22 @@ class PokemonsController < ApplicationController
   
   def capture
       pokemon = Pokemon.find(params[:id])
-      puts pokemon
+      #puts pokemon
       pokemon.trainer_id = current_trainer.id
-      puts current_trainer
-      puts "HHHHHHAAAGHHA"
+      #puts current_trainer
+      #puts "HHHHHHAAAGHHA"
       pokemon.save
       redirect_to '/'
+  end
+
+  def damage
+      pokemon = Pokemon.find(params[:id])
+      pokemon.health = pokemon.health - 10
+      pokemon.save
+      if pokemon.health <= 0
+         pokemon.destroy
+      end      
+      path = '/trainers/' + current_trainer.id.to_s
+      redirect_to path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   patch 'capture', to: 'pokemons#capture'
+
+  patch 'damage', to: 'pokemons#damage'
+
   devise_for :trainers
   resources :trainers
 end
